55cadee1af9924ee05c703f457dcc1b601395dd9
Jean Renel Fanfan <131271711+jrfanfan@users.noreply.github.com>
2024-07-18 09:30:14 -0400

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 
 // Callback URL for handling the OAuth 2.0 response
 router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
     // Successful authentication, redirect or handle the user as desired
-    res.redirect('http://localhost:8080/dashboard');
+    res.redirect('https://cse341-finalexam.onrender.com/dashboard');
   });
   
   router.get('/logout', function(req, res, next) {
